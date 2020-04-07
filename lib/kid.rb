572d3cdcd37b26e7e9fb7a0473a1655a2da0d788
Diane Crame <dcrame@icloud.com>
2020-04-07 22:38:18 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,10 @@
-require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
+require_relative "../lib/kid.rb"
+require_relative "../lib/dancer.rb"
+
+class Kid
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end
